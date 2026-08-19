4643d81ae20ac58a056ce1d4b7d3bd5c91ddd5d5
Petro Krevenets <peter.k.nets@gmail.com>
2026-08-19 10:37:56 +0300

    Add placeholder data

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,20 @@
 import { Injectable } from '@nestjs/common';
 
+import type { User } from './users.type';
+
+// FIXME: mockup, replace with a real database
+const users: User[] = [
+  {
+    userId: 1,
+    username: 'Alice',
+    password: 'topsecret', // FIXME: use a hash instead
+  },
+  {
+    userId: 2,
+    username: 'Bob',
+    password: '123abc',
+  },
+];
+
 @Injectable()
 export class UsersService {}
